31bb6d948fc1cf421c60482f16df1215c43758dd
kripedia <krispediadot@gmail.com>
2019-03-21 04:32:39 +0900

    add getDistance

--- assign02.cpp
+++ assign02.cpp
@@ -4,6 +4,18 @@
 
 using namespace std;
 
+int getDistance(pair<int,int> from, pair<int,int> to){
+    int result;
+    result = abs(from.first-to.first) + abs(from.second-to.second);
+
+    return result;
+}
+
+int TPS(vector<pair<int,int> >, int N){
+    
+    return 0;
+}
+
 int main(void) {
     ios_base::sync_with_stdio(false);
     int N;
@@ -27,5 +39,7 @@ int main(void) {
     for(auto &p: pos)
         cout<<p.first<<" "<<p.second<<endl;
 
+    int result = getDistance(pos[0], pos[3]);
+    cout<<result<<endl;
     return 0;
 }
