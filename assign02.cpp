8988557bfed898bd8a6c42a07c33f98892fc0799
kripedia <krispediadot@gmail.com>
2019-03-21 04:23:21 +0900

    read file & create vector<pair<>> structure

--- assign02.cpp
+++ assign02.cpp
@@ -7,20 +7,25 @@ using namespace std;
 int main(void) {
     ios_base::sync_with_stdio(false);
     int N;
+    int x,y;
     string line;
     vector<pair<int, int> > pos;
 
+    // Read file & Save into vector<pair<>> structure
     fstream fin;
     fin.open("input.txt");
     fin>>N;
+
     for(int i=0; i<N; i++){
-        fin>>pos[i].first>>pos[i].second;
+        fin>>x>>y;
+        pos.push_back(make_pair(x,y));
     }
     fin.close();
 
+    // Check input data 
     cout<<N<<endl;
     for(auto &p: pos)
-        cout<<p.first<<p.second<<endl;
+        cout<<p.first<<" "<<p.second<<endl;
 
     return 0;
 }
