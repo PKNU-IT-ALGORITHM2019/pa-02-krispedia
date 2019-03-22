c371c327779e24b14dc80300533dbc5483af02dc
kripedia <krispediadot@gmail.com>
2019-03-23 00:45:02 +0900

    add assign02

--- assign02.cpp
+++ assign02.cpp
@@ -1,111 +1,139 @@
-#include <iostream> // for cin/cout
-#include <fstream>  // for file input
-#include <vector>   // for vector
-#include <map>      // for map
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <map>
+#include <cmath>
+#include <climits>
+#include <stack>
+#include <ctime>
 
 using namespace std;
 
-vector <int> route;
-map<pair<int,int>,int> pos;
+int N;
 
-void routePrint(vector<int> route);
-int getDistance(pair<int,int> from, pair<int,int> to);
-int TSP(pair<int,int> check, multimap<int, pair<int,int> > loc, int n);
+map<int,pair<int,int> > node;
+vector<bool> visited;
+stack<int> tour;
+vector<int> tour_temp;
+stack<int> minTour;
+double minDist = numeric_limits<double>::max();
 
-// Function to convert a std::map<K,V> to std::multimap<V,K>
-template<typename K, typename V>
-multimap<V,K> invertMap(map<K,V> const &map)
-{
-	multimap<V,K> multimap;
-
-	for (auto const &pair: map) {
-		multimap.insert(make_pair(pair.second, pair.first));
-	}
-
-	return multimap;
+// Read file & make node vector
+void readFile(string fileName){
+    int x,y;
+    fstream fin;
+    fin.open(fileName);
+    fin>>N;
+    for(int i=0; i<N; i++){
+        fin>>x>>y;
+        pair<int,int> temp = make_pair(x,y);
+        node[i] = temp;
+    }
+    fin.close();
 }
-
-void routePrint(vector<int> route){
+// Init vector size
+void init(){
+    visited = vector<bool> (N, false); 
+}
+double getDistance(pair<int,int> node1, pair<int,int> node2){
+    return sqrt(pow(node1.first-node2.first, 2.0) + pow(node1.second-node2.second, 2.0));
+}
+void printMinTour(){
     cout<<"[ ";
-    for(auto it=route.begin(); it!=route.end(); ++it)
-        cout<<*it<<" ";
+    for(int i=0; i<N; i++){
+        cout<<minTour.top()<<' ';
+        minTour.pop();
+    }
     cout<<"]"<<'\n';
 }
-
-int getIndex(map<pair<int,int>,int> pos, pair<int,int> check){
-    
-    return pos.find(check)->second;
+void printVisited() {
+    cout<<"--visited--"<<'\n';
+    for(auto it = visited.begin(); it!=visited.end(); ++it)
+        cout<<*it<<' ';
+    cout<<'\n';
 }
-
-int getDistance(pair<int,int> from, pair<int,int> to){
-    int result;
-    result = abs(from.first-to.first) + abs(from.second-to.second);
-
-    return result;
-}
-
-int TSP(pair<int,int> check, multimap<int, pair<int,int> > loc, int n){
-    if(n==1){
-        route.push_back(loc.begin()->first);
-        return 0;
+void switchTour(){
+    tour_temp.clear();
+    for(int i=0; i<N; i++){
+        int temp;
+        temp = tour.top();
+//        cout<<"tour.top(): "<<temp<<'\n';
+        minTour.push(temp);
+//        cout<<"minTour.push(): "<<temp<<'\n';
+        tour_temp.push_back(temp);
+//        cout<<"tour_temp["<<i<<"]"<<tour_temp[i]<<'\n';
+        tour.pop();
+    }  
+//    cout<<"minTour.top(): "<<minTour.top()<<'\n';
+    for(auto it=tour_temp.end()-1; it!=tour_temp.begin(); --it){
+        tour.push(*it);
+//        cout<<"tour pushed: "<<*it;
     }
-    int dist;
-    int min=0;
-    pair<int,int> checkNext;
-
-    for(auto it=loc.begin(); it!=loc.end(); ++it){
-        dist = getDistance(check, it->second);
-        if(dist < min){
-            min = dist;
-            checkNext = it->second;
+    tour.push(*tour_temp.begin());
+//    cout<<'\n';
+}
+void TSP(int checkIndex, int n, double dist){
+    cout<<"TSP START!"<<'\n';
+    if(n == 0){
+        double checkToStart = getDistance(node.find(checkIndex)->second, node.find(0)->second);
+        dist += checkToStart;
+        tour.push(checkIndex);
+//        cout<<"final stack pushed: "<<checkIndex<<'\n';
+//        cout<<'\n'<<"dist: "<<dist<<'\n';
+//        cout<<"minDist: "<<minDist<<'\n'<<'\n';
+        if(dist<minDist){
+//            cout<<"minDist change!!"<<'\n';
+            minDist = dist;
+//            cout<<"switch Tour!!"<<'\n';
+            switchTour();
+//            cout<<"Switch Tour dond!!"<<'\n';
         }
+        visited[checkIndex] = false;
+//        cout<<"stack poped: "<<tour.top()<<'\n';
+        tour.pop();
+//        printVisited();    
+//        cout<<'\n';
     }
-    // Find min dist Index via pair<int,int>
-    int index = getIndex(pos, checkNext);
+    else{
+        visited[checkIndex] = true;
+        tour.push(checkIndex);
+//        cout<<"stack pushed: "<<checkIndex<<'\n';
+//        printVisited();
 
-    // Push min dist Index 
-    route.push_back(index);
-    loc.erase(index);
-    
-    return TSP(checkNext, loc, n-1);
-
+        for(int i=0; i<N; i++){
+            if(i == checkIndex)
+                continue;
+            if(visited[i] == false){
+                double checkToI = getDistance(node.find(checkIndex)->second, node.find(i)->second);
+                TSP(i, n-1, dist+checkToI);
+            }
+        }
+        visited[checkIndex] = false;
+        tour.pop();
+//        cout<<"stack poped: "<<checkIndex<<'\n';
+//        printVisited();
+    }
 }
 
 int main(void) {
     ios_base::sync_with_stdio(false);
-    int N;
-    int x,y;
-    string line;
+    time_t start, end;
+    start = time(NULL);
 
-    // Read file & Save into vector<pair<>> structure
-    fstream fin;
-    fin.open("input.txt");
-    fin>>N;
+	readFile("input4.txt");
 
-    for(int i=0; i<N; i++){
-        fin>>x>>y;
-        pos[make_pair(x,y)] = i;
-    }
-    fin.close();
+    init();
+
+    TSP(0, N-1, 0);    
     
-    multimap<int,pair<int,int> > multimap = invertMap(pos);
+    end = time(NULL);
+    double result = (double)(end-start);
+    cout<<"걸린 시간: "<<result<<"s"<<'\n';
 
-    // print the multimap
-	for (auto const &pair: multimap) {
-		cout << '{' << pair.second.first << pair.second.second << "," << pair.first << '}' << '\n';
-	}
+    cout<<fixed;
+    cout.precision(14);
+    cout<<"최소 거리: "<<minDist<<'\n';
+    printMinTour();
 
-    cout<<"multimap.begin()->first: "<<multimap.begin()->first<<'\n';
-    cout<<"multimap.begin()->second: "<<multimap.begin()->second.first<<'\n';
-    cout<<"multimap.begin()->second: "<<multimap.begin()->second.second<<'\n';
-
-    route.push_back(multimap.begin()->first);
-    multimap.erase(multimap.begin());
-
-    TSP(multimap.begin()->second, multimap, N-1);
-    
-    routePrint(route);
-
-    // Check input data 
     return 0;
-    }
+}
